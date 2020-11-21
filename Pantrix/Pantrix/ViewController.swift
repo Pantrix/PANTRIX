4712c4a784e58b05a407a0a830d98f6a8cdaa062
Sumaiya Ahmed <sumaiyahmed2848@gmail.com>
2020-11-21 15:57:13 -0500

    changes

---
+++ Pantrix/Pantrix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pantrix
+//
+//  Created by Sumaiya Ahmed on 11/21/20.
+//  Copyright © 2020 pantrix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
