8fa8221dad0823d30c052bd5ce3173ae0f260b4b
Sumaiya Ahmed <sumaiyahmed2848@gmail.com>
2020-11-21 18:37:09 -0500

    Login Test

---
+++ Pantrix/Pantrix/LoginViewController.swift
@@ -0,0 +1,43 @@
+//
+//  LoginViewController.swift
+//  Pantrix
+//
+//  Created by Sumaiya Ahmed on 11/21/20.
+//  Copyright © 2020 pantrix. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    @IBAction func onSignIn(_ sender: Any) {
+    }
+    @IBAction func onSignup(_ sender: Any) {
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
